9d524f82f87d6c6c54ce7f36167486473c8e3da3
Mike Dreiling <mdarts@gmail.com>
2019-01-10 13:09:54 -0700

    first commit

---
+++ src/components/ForeCastContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="foreCastContainer">
+    <div class="foreCastStrip">
+      ForeCast Stuff
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForeCastContainer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mainContainer">
+    <InfoDrawer />
+    <ForeCastContainer />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './Footer.vue'
+import ForeCastContainer from './ForeCastContainer.vue'
+import InfoDrawer from './InfoDrawer.vue'
+
+export default {
+  name: 'MainContainer',
+components: {
+  InfoDrawer,
+  ForeCastContainer,
+  Footer
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
